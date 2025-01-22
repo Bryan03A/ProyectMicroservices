2a14a880b1807266f860a5eb72683b4a7ecb054b
DavCoder22 <davidelpatron22@gmail.com>
2025-01-22 15:53:03 -0500

    fix: fix go.mod
    error fixed about go dependences
    
    any

--- search-and-filters-service/    main.go
+++ search-and-filters-service/    main.go
@@ -2,16 +2,17 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    _ "search-and-filters-service/config"
-    "search-and-filters-service/handlers"
+	_ "search-and-filters-service/config"
+	"search-and-filters-service/handlers"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    r := gin.Default()
+	r := gin.Default()
 
-    r.POST("/index", handlers.IndexModel)
-    r.GET("/search", handlers.SearchModels)
+	r.POST("/index", handlers.IndexModel)
+	r.GET("/search", handlers.SearchModels)
 
-    r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
